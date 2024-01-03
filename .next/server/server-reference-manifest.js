6173491106343e12c1fdf73954bd81d71d578e46
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-01-03 19:45:44 -0300

    Add new files and update configuration

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iPSyRb3VoMO+HGTqhesx9JGeiV/lJq69f6Vlumovg5U=\"\n}"
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1> hello world</h1>
+      <Button>click</Button>
+    </main>
+  );
+}
