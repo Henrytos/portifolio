e1a651d5c1eea710f1f43163ccda6ba157941f24
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-01-03 22:45:50 -0300

    Update project page heading

--- my-app/src/app/projects/page.tsx
+++ my-app/src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="">
-      <h1 className="text-"> hello world projects</h1>
+      <h1 className="text-"> hello world projects ;-;</h1>
       <Button>click</Button>
       <ThemeToggle />
     </main>
