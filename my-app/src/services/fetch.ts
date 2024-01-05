215ae0c13e92b8e9dfad348badd4ab8b7d4caced
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-01-05 18:48:55 -0300

    tratamentos de erros

--- my-app/src/services/fetch.ts
+++ my-app/src/services/fetch.ts
@@ -2,6 +2,17 @@ import { ProjectType } from "@/types/type-response";
 
 export async function fetchProjects(): Promise<ProjectType[]> {
   const data = await fetch(`${process.env.API_URL}/projects`);
-  const projects: ProjectType[] = await data.json();
-  return projects;
+
+  if (!data) {
+    console.error("Failed to fetch projects. Status:");
+    return [];
+  }
+
+  try {
+    const projects: ProjectType[] = await data.json();
+    return projects;
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+    return [];
+  }
 }
