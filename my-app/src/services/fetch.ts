078e1b0e413817119e1522af280c15801dd9fd32
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-01-05 18:43:18 -0300

    será

--- my-app/src/services/fetch.ts
+++ my-app/src/services/fetch.ts
@@ -1,7 +1,7 @@
 import { ProjectType } from "@/types/type-response";
 
 export async function fetchProjects(): Promise<ProjectType[]> {
-  const data = await fetch(`${process.env.API}/projects`);
+  const data = await fetch(`${process.env.API_URL}/projects`);
   const projects: ProjectType[] = await data.json();
   return projects;
 }
