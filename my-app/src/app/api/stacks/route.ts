4720fce30fa377ecf612460c89edb8df897678fb
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-01-08 17:37:04 -0300

    deletanto api

--- my-app/src/app/api/stacks/route.ts
+++
@@ -1,8 +0,0 @@
-import { StackResponseType, StackType } from "@/types/type-response";
-import database from "../../../../database.json";
-
-export async function GET() {
-  const { stacks } = database;
-  const response = stacks;
-  return Response.json(response);
-}
